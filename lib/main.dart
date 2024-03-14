9d48f2909e0e838073cc9db8483564019733f622
DemonHashira <v.logodazhki@outlook.com>
2024-03-14 14:15:51 +0200

    Added buttons and modes to auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'FlutterChat',
       theme: ThemeData().copyWith(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        ),
       ),
       home: const AuthScreen(),
     );
