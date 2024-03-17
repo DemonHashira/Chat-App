8b5738f01e699e58bf6119a83f555efa54681a71
DemonHashira <v.logodazhki@outlook.com>
2024-03-17 19:12:25 +0200

    Managed the selected image in the form

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -4,7 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key});
+  const UserImagePicker({
+    super.key,
+    required this.onImagePicked,
+  });
+
+  final void Function(File pickedImage) onImagePicked;
 
   @override
   State<StatefulWidget> createState() {
@@ -29,6 +34,8 @@ class _UserImagePickerState extends State<UserImagePicker> {
     setState(() {
       _pickedImage = File(pickedImage.path);
     });
+
+    widget.onImagePicked(_pickedImage!);
   }
 
   @override
